61271754cd0111b02d118fff07e1be67c35a5f83
Gourav Ghuge <you@example.com>
2024-07-13 20:55:44 +0530

    update the code

--- day1_data_types/activity2.js
+++ day1_data_types/activity2.js
@@ -4,10 +4,58 @@
 
 
 
-var myVar = "hello";
-console.log(typeof myVar);
+// var myVar = "hello";
+// console.log(typeof myVar); // string
 
-var myVar = true;
-console.log(typeof myVar);
+// var myVar = true;
+// console.log(typeof myVar);  // boolean
 
-console.log(typeof myVar);
+
+let number = 10;  // Number type
+console.log(typeof number);  // number
+
+
+let string = "hello";  // String type
+console.log(typeof string);  // string
+
+let boolean = true;  // Boolean type
+console.log(typeof boolean);  // boolean
+
+let undef = undefined;  // Undefined type
+console.log(typeof undef);
+
+
+let nullData = null;  // Null type
+console.log(typeof nullData);
+
+let object = {};  // Object type
+console.log(typeof object);
+
+let array = [];  // Array type
+console.log(typeof array);
+
+
+let function1 = function() {};  // Function type
+console.log(typeof function1);
+
+
+
+
+
+
+let number2 = 10;  // Number type
+
+console.log(number2);
+
+number2 = 20;
+console.log(number2);
+
+
+
+
+const name = 'John Doe';
+
+console.log(name);
+
+// name = 'Jane Doe';  //// this line will not work give constant error assign to constant
+console.log(name);
--- day1_data_types/index.ts
+++ day1_data_types/index.ts
@@ -7,3 +7,53 @@ const way = 'Hello World!';
 
 console.log(way);
 
+
+//// typescript data types ////
+
+let demoName:string = 'gaurav ghuge ';
+
+console.log(demoName);
+
+let number1:Number = 290;
+console.log(number1);
+
+let boolean1:boolean = true;
+console.log(boolean1);
+
+// let boolean2:boolean = "true";  // this is wrong we can't      //  "true" => true
+
+
+let boolean2:boolean | string | Number = "true";  // this is right       //  "true" => true
+
+console.log(boolean1);
+
+let data2 : string |number | boolean | null | undefined = null;
+console.log(data2);
+
+
+data2 = "gaurav ghuge"
+console.log(data2);
+
+
+
+
+
+
+interface Person {
+    name: string;
+    age: number;
+  }
+  let person: Person = { name: "John", age: 25 };
+
+  console.log(person);
+  console.log(typeof person);
+interface Car {
+    demo: string;
+    demo2: Number;
+    // demo3: Car;
+}
+
+let car:Car = {demo:"audi", demo2:20,};
+
+console.log(car);
+
